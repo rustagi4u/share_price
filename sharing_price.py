7502e970ad356a8c8b2c94e13d4d01b7246f4e16
ankit rustagi <ankit.rustagi@3pillar.com>
2013-11-29 11:56:35 +0530

    Share Price Code

---
+++ sharing_price.py
@@ -0,0 +1,28 @@
+import csv
+
+def return_max():
+    f = open("sharing_price.csv", "rb")
+    csv_reader = csv.reader(f)
+    header = csv_reader.next()
+    complete_data = {}
+    for i in range(2, len(header)):
+        complete_data[header[i]] = {}
+    
+    for r in csv_reader:
+        values = r
+        
+        for i in range(2, len(values)):
+    #        data_dict[r[0]+" "+r[1]] = values[2:]
+            complete_data[header[i]][r[0]+" "+r[1]] = values[i]
+    
+    max_values ={}
+    for key, values in complete_data.iteritems():
+        max_value = max(values.iterkeys(), key= (lambda key: int(values[key])))
+        max_values[key] = max_value
+     
+    return max_values
+#    data_dict = {}
+#    print (r[0], r[1], max_value)
+
+if __name__ == "__main__":
+    print return_max()
